c94d15a9ff8cba34ed42a38120561777bbb074ce
GreenBean0303 <lootsmannagnes@gmail.com>
2025-10-15 09:52:32 +0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from "react";
+import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3050/api/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
   return (
-    <div className="app">
-      <h1>Postituste rakendus</h1>
-      {posts.map(post => (
-        <div key={post.id} style={{margin: '20px', padding: '10px', border: '1px solid #ccc'}}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-          <small>Autor: {post.author}</small>
-          <h4>Kommentaarid:</h4>
-          {post.comments.map(comment => (
-            <div key={comment.id} style={{marginLeft: '20px', padding: '5px'}}>
-              <strong>{comment.author}:</strong> {comment.content}
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className="container">
+      <h1>Loo postitus</h1>
+      <PostCreate />
+      <hr />
+      <h1>Postitused</h1>
+      <PostList />
     </div>
   );
 }
